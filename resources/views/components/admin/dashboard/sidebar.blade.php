2b983a2a43f135ac00ba4ed3b6978a9ed8a8f37c
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-05-21 23:59:28 +0530

    ADD:asset folder

---
+++ resources/views/components/admin/dashboard/sidebar.blade.php
@@ -0,0 +1,36 @@
+        <div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="menu-title">
+                            <span>Menu</span>
+                        </li>
+
+
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-graduation-cap"></i> <span>User</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="#">User List</a></li>
+                                <li><a href="#">User Add</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-chalkboard-teacher"></i> <span>Service Provider</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="#">Service Provider List</a></li>
+                                <li><a href="/service-provider/create">Service Provider Add</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-building"></i> <span>Compliant</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="#">Compliant List</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
---
+++ resources/views/components/service_provider/dashboard/sidebar.blade.php
@@ -0,0 +1,32 @@
+        <div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="menu-title">
+                            <span>Menu</span>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-graduation-cap"></i> <span>Book</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="#">Book List</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-chalkboard-teacher"></i> <span>Service Provider</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="#">Service Provider Update</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-building"></i> <span>Compliant</span> <span
+                                    class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="departments.html">Compliant List</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
