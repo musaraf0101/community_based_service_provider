f092979b4688121c513d0492153ac04343164c6c
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-05-31 20:21:55 +0530

    home view create

---
+++ resources/views/common_pages/home_view.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('section')
+@include('components.common.main_components.navbar')
+@include('components.common.home')
+@include('components.common.main_components.footer')
+@endsection
