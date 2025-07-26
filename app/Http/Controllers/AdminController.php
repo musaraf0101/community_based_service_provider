1e462b00f2d023fc712f111b594940d051f26216
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-26 09:21:34 +0530

    add counting function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use App\Models\Compliant;
+use App\Models\ServiceProvider;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-     public function index()
+    public function index()
     {
         return view('admin_pages.admin_dashboard_view');
     }
@@ -15,5 +18,32 @@ class AdminController extends Controller
     {
         $compliants = Compliant::all();
         return view('admin_pages.compliant_list_view', compact('compliants'));
+    }
+    public function adminDashboard()
+    {
+        $totalUsers = User::count();
+        $totalBookings = Booking::count();
+        $totalComplaints = Compliant::count();
+        $totalServiceProviders = ServiceProvider::count();
+        $maleProviders = ServiceProvider::where('gender', 'male')->count();
+        $femaleProviders = ServiceProvider::where('gender', 'female')->count();
+        $otherProviders = ServiceProvider::where('gender', 'other')->count();
+
+        $userCount = User::where('role','user')->count();
+        $serviceProviderCount = ServiceProvider::where('role','service_provider')->count();
+
+
+
+        return view('admin_pages.admin_dashboard_view', compact(
+            'totalUsers',
+            'totalBookings',
+            'totalComplaints',
+            'totalServiceProviders',
+            'maleProviders',
+            'femaleProviders',
+            'otherProviders',
+            'usercount',
+            'serviceProviderCount',
+        ));
     }
 }
