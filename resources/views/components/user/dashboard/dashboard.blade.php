cf4dc44ada46cebe45f6b9684602e4cd4b289d7c
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-26 16:06:31 +0530

    dashboard content added

---
+++ resources/views/components/user/dashboard/dashboard.blade.php
@@ -0,0 +1,33 @@
+
+<div class="row">
+    <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card inovices-card card-total-booking">
+            <div class="card-body">
+                <div class="inovices-widget-header">
+                    <span class="inovices-widget-icon">
+                        <img src="assets/img/icons/invoices-icon2.svg" alt="" />
+                    </span>
+                    <div class="inovices-dash-count">
+                        <div class="inovices-amount">12</div>
+                    </div>
+                </div>
+                <p class="inovices-all">Total Booking's</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card inovices-card total-compliant">
+            <div class="card-body">
+                <div class="inovices-widget-header">
+                    <span class="inovices-widget-icon">
+                        <img src="assets/img/icons/invoices-icon2.svg" alt="" />
+                    </span>
+                    <div class="inovices-dash-count">
+                        <div class="inovices-amount">3</div>
+                    </div>
+                </div>
+                <p class="inovices-all">Total Compliant's</p>
+            </div>
+        </div>
+    </div>
+</div>
